23ed55254e38bb53d0f21b4a2d980f4ea73193b5
Sumanta Parida <sumanta90parida@gmail.com>
2023-07-08 12:32:40 +0530

    chore: Stope twice render

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = withBundleAnalyzer({
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
-  reactStrictMode: true,
+  reactStrictMode: false,
   compiler: {
     styledComponents: true,
   },
